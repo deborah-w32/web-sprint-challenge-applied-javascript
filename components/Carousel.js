d7e8f6c52e4aa87d236c521da298a8af507a3538
Deborah Warren <deborah.dw.32@gmail.com>
2020-12-13 21:41:51 -0500

    Carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,69 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const entryPoint = document.querySelector('div.carousel-container');
+
+function carouselMaker(){
+
+  const carousel = document.createElement('div');
+  const leftButton = document.createElement('div');
+  const carouselImgMountains = document.createElement('img');
+  const carouselImgComputer = document.createElement('img');
+  const carouselImgTrees = document.createElement('img');
+  const carouselImgTurntable = document.createElement('img');
+  const rightButton = document.createElement('div');
+
+  carousel.className = 'carousel';
+  leftButton.className = 'left-button';
+  rightButton.className= 'right-button';
+
+  leftButton.textContent = '<';
+  carouselImgMountains.setAttribute('src', './assets/carousel/mountains.jpeg');
+  carouselImgComputer.setAttribute('src', './assets/carousel/computer.jpeg');
+  carouselImgTrees.setAttribute('src', './assets/carousel/trees.jpeg');
+  carouselImgTurntable.setAttribute('src', './assets/carousel/turntable.jpeg');
+  rightButton.textContent = '>';
+
+  entryPoint.appendChild(carousel);
+  carousel.appendChild(leftButton);
+  carousel.appendChild(carouselImgMountains);
+  carousel.appendChild(carouselImgComputer);
+  carousel.appendChild(carouselImgTrees);
+  carousel.appendChild(carouselImgTurntable);
+  carousel.appendChild(rightButton);
+
+  let images = [carouselImgMountains, carouselImgComputer, carouselImgTrees,
+  carouselImgTurntable];
+  let index = 0;
+  let currentImg = images[index];
+  currentImg.style.display = 'block';
+
+  leftButton.addEventListener('click', (e) => {
+    currentImg.style.display = 'none';
+    let imgBack = index - 1;
+    if (imgBack < 0){
+      imgBack = 3;
+      index = 3;
+    }
+    index = imgBack;
+    currentImg = images[imgBack];
+    currentImg.style.display = 'block';
+  });
+
+  rightButton.addEventListener('click', (e) => {
+    currentImg.style.display = 'none';
+    let imgForward = index + 1;
+    if (imgForward > 3){
+      imgForward = 0;
+      index = 0;
+    }
+    index = imgForward;
+    currentImg = images[imgForward];
+    currentImg.style.display = 'block';
+  });
+
+  return carousel;
+}
+
+carouselMaker();
